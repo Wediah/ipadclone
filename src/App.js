182cfb5a16efcd812f9d34066013b1ee687ee413
Wediah <100675006+Wediah@users.noreply.github.com>
2023-08-27 00:03:30 +0000

    revisiting react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {MdKeyboardArrowRight} from 'react-icons/md'
+import {TfiArrowCircleRight} from 'react-icons/tfi'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black h-screen">
+      <div>
+        <h3 className="text-white text-center p-2 flex justify-center bg-gray-800 items-center">Save on iPad or Mac with education pricing. Plus get a gift card up to $150. <span className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer'> Shop now<MdKeyboardArrowRight/></span></h3>
+      </div>
+
+      <div className='flex '>
+          <div className=''>
+            <img src='/images/apple2.png' alt='apple ipad with final cut' className='object-left'/>
+          </div>
+          <div className='w-96'> 
+            <h2 className='justify-center flex text-center text-2xl font-semibold text-white'>Introducing</h2>
+            <h1 className=' flex items-center text-center justify-center text-4xl font-bold text-white'>Final Cut Pro <br/> and Logic Pro <br/> for iPad</h1>
+            <p className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer justify-center'>Learn more about Final Cut Pro for iPad <MdKeyboardArrowRight/></p>
+            <p className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer justify-center'>Learn more about Logic Pro for iPad <MdKeyboardArrowRight/></p>
+            <p className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer justify-center gap-1'>watch the film <TfiArrowCircleRight/></p>
+          </div>
+          <div>
+            <img   src='/images/apple3.png' alt='apple ipad with final cut'/>
+          </div>
+      </div>
+
     </div>
   );
 }
