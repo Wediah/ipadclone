4391f47081affe8b431ba0480ea6273476933aef
Wediah <100675006+Wediah@users.noreply.github.com>
2023-09-02 14:44:10 +0000

    landing page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import {MdKeyboardArrowRight} from 'react-icons/md'
-import {TfiArrowCircleRight} from 'react-icons/tfi'
+
 
 function App() {
   return (
-    <div className="bg-black h-screen">
-      <div>
-        <h3 className="text-white text-center p-2 flex justify-center bg-gray-800 items-center">Save on iPad or Mac with education pricing. Plus get a gift card up to $150. <span className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer'> Shop now<MdKeyboardArrowRight/></span></h3>
+    <div className=" min-h-screen bg-gray-50">
+      <div className='flex flex-row gap-10 justify-center items-center mx-10 pt-32 '>
+        <div>
+          <h1 className='font-bold text-9xl text-violet-900'>PayWise</h1>
+          <h1 className="font-medium text-4xl italic pt-5">No need to look over your shoulders <br/> when spending your money. </h1>
+          <p className="font-thin text-2xl">Spending, saving, investing and sending money can be a daunting task <br/> especially when you have a lot on your mind. But with our tools <br/> and resources, you can do all of that without any noise or sweat.</p>
+          <p className="font-medium text-xl">As the saying goes, L'argent n'aime pas le bruit. Sika mp3 dede.</p>
+          <button className="p-3 bg-violet-900 text-white rounded-full mt-2">Get Started</button>
+        </div>
+        <div className='bg-gradient-to-t from-violet-700 to-gray-200 rounded-full w-1/3 h-1/2'> 
+          <img className='' src='/images/zzz2.png' alt=""/>
+        </div>
+        
       </div>
 
-      <div className='flex '>
-          <div className=''>
-            <img src='/images/apple2.png' alt='apple ipad with final cut' className='object-left'/>
-          </div>
-          <div className='w-96'> 
-            <h2 className='justify-center flex text-center text-2xl font-semibold text-white'>Introducing</h2>
-            <h1 className=' flex items-center text-center justify-center text-4xl font-bold text-white'>Final Cut Pro <br/> and Logic Pro <br/> for iPad</h1>
-            <p className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer justify-center'>Learn more about Final Cut Pro for iPad <MdKeyboardArrowRight/></p>
-            <p className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer justify-center'>Learn more about Logic Pro for iPad <MdKeyboardArrowRight/></p>
-            <p className='text-blue-800 flex items-center hover:underline font-semibold cursor-pointer justify-center gap-1'>watch the film <TfiArrowCircleRight/></p>
-          </div>
-          <div>
-            <img   src='/images/apple3.png' alt='apple ipad with final cut'/>
-          </div>
-      </div>
+      <h1 className="font-bold text-center pt-8 text-3xl ">Ho<span className="underline underline-offset-4 decoration-violet-500 decoration-4">w It Wor</span>ks</h1>
 
+      
+      
+     
     </div>
+    
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav className=" fixed w-full z-10 bg-gray-50">
+            <div className="w-full">
+                <div className="flex items-center h-20 w-full">
+                    <div className="flex items-center  mx-20  justify-between w-full">
+                        <div className="flex justify-center items-center flex-shrink-0 ">
+                            
+                            <div>
+                              <h1 className=" font-bold text-xl cursor-pointer -ml-12 font-Zen Dots">
+                                <span className="">Pay</span>
+                                <span className="text-violet-500">Wise</span>
+                            </h1>  
+                            </div>
+                            
+                        </div>
+                        <div className="hidden md:block">
+                            <div className="ml-10 flex items-baseline space-x-4 font-medium cursor-pointer ">
+                                
+                                <div className=' text-black hover:text-violet-900'>
+                                    Discover
+                                </div>
+                                <div className=' text-black hover:text-violet-900'>
+                                    Features
+                                </div>
+                                <div className=' text-black hover:text-violet-900'>
+                                    Partners
+                                </div>
+                                <div className=' text-black hover:text-violet-900'>
+                                    About us
+                                </div>
+
+                                <div className=' text-black hover:text-violet-900'>
+                                    Contact
+                                </div>
+                                
+
+                            </div>
+                            
+                        </div>
+                        <div className='font-medium text-white bg-violet-900 p-2 rounded-full cursor-pointer hover:bg-indigo-700'> 
+                            Get Started
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            </div>
+
+            
+            
+        </nav>
+  )
+}
+
+export default Nav
